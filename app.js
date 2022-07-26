88ab06a15e6aa58f8d43bf3fd47f7fede017d305
Simanta Paul <simanta.paul.26@gmail.com>
2022-07-27 02:52:17 +0600

    included cash in function

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-/* eslint-disable linebreak-style */
 const fs = require('fs');
+const { promisify } = require('util');
 
-console.log(process.argv[2]);
+const readFileAsync = promisify(fs.readFile);
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) console.log(err);
-    console.log(JSON.parse(data));
-});
+const printCommisions = async () => {
+    try {
+        const data = await readFileAsync(process.argv[2], 'utf-8');
+        const inputs = JSON.parse(data);
+        inputs.forEach((item) => {
+            if (item.type === 'cash_in') {
+                const amount = item?.operation?.amount || 0;
+                const commission = (amount * 0.03) / 100;
+                process.stdout.write(`${commission <= 5.00 ? commission : 5.00}\n`);
+            }
+        });
+    } catch (err) {
+        process.stdout.write(err.message);
+    }
+};
+
+printCommisions();
