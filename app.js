e8f3a8d13214989a6b2a5a3e3ec00651512f1256
Simanta Paul <simanta.paul.26@gmail.com>
2022-07-28 20:39:08 +0600

    applied factory pattern

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-nested-ternary */
 const fs = require('fs');
 const { promisify } = require('util');
-const commissionData = require('./lib/config');
+const { createTransaction } = require('./transaction');
 
 const readFileAsync = promisify(fs.readFile);
 
@@ -10,19 +10,8 @@ const printCommisions = async () => {
         const data = await readFileAsync(process.argv[2], 'utf-8');
         const inputs = JSON.parse(data);
         inputs.forEach((item) => {
-            const amount = item?.operation?.amount || 0;
-            if (item.type === 'cash_in') {
-                const max = commissionData[item.type][item.user_type].max.amount;
-                const commission = (amount * commissionData[item.type][item.user_type].percents) / 100;
-                process.stdout.write(`${commission <= max ? commission : max}\n`);
-            }
-            if (item.type === 'cash_out') {
-                const limit = commissionData[item.type][item.user_type].week_limit.amount;
-                const min = commissionData[item.type][item.user_type].min.amount;
-                const cutAmount = limit ? (amount >= limit ? amount - limit : 0) : amount;
-                const commission = (cutAmount * commissionData[item.type][item.user_type].percents) / 100;
-                process.stdout.write(`${commission < min ? min : commission}\n`);
-            }
+            const transaction = createTransaction({ transaction: item });
+            process.stdout.write(transaction.getCommision());
         });
     } catch (err) {
         process.stdout.write(err.message);
---
+++ transaction.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-case-declarations */
+const commissionData = require('./lib/user.config');
+
+class Transaction {
+    constructor(transaction) {
+        this.type = transaction.type;
+        this.userType = transaction.user_type;
+        this.amount = transaction?.operation?.amount || 0;
+    }
+
+    getCommision() {
+        let commission = 0.00;
+        switch (this.type) {
+            case 'cash_in':
+                const max = commissionData[this.type][this.userType].max.amount;
+                commission = (this.amount * commissionData[this.type][this.userType].percents) / 100;
+                return `${commission <= max ? commission : max}\n`;
+            case 'cash_out':
+                const limit = commissionData[this.type][this.userType].week_limit.amount;
+                const min = commissionData[this.type][this.userType].min.amount;
+                const cutAmount = limit ? (this.amount >= limit ? this.amount - limit : 0) : this.amount;
+                commission = (cutAmount * commissionData[this.type][this.userType].percents) / 100;
+                return `${commission < min ? min : commission}\n`;
+            default:
+                return process.stdout.write('Invalid Transaction Type!');
+        }
+    }
+}
+
+module.exports.createTransaction = ({ transaction }) => new Transaction(transaction);
